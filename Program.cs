5827bf6ba583739017fa0d643fae511e1c116c22
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-30 17:39:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InheritanceDemo
+{
+    class Student
+    {
+        string firstname, identity;
+        public Student(string firstname, string identity)
+        {
+            this.firstname = firstname;
+            this.identity = identity;
+        }
+        ~Student()
+        {
+
+        }
+    }
+    class HostelStudent : Student
+    {
+        int roomno;
+        public HostelStudent(string firstname, string identity, int roomno) : base(firstname, identity)
+        {
+            this.roomno = roomno;
+            PrintDetails(firstname, identity, roomno);
+        }
+
+        public void PrintDetails(string firstname, string identity, int roomno)
+        {
+            Console.WriteLine("First Name: " + firstname);
+            Console.WriteLine("Identity: " + identity);
+            Console.WriteLine("Room No.: " + roomno);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            new HostelStudent("ABC", "ID001", 1);
+        }
+    }
+}
